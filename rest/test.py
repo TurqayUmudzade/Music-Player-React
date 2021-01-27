148cc4e15ed8895a2a8efca21a321ae91c4c2c96
Turqay Umudzade <turgayau@code.edu.az>
2021-01-27 18:09:37 +0400

    folder clean up

--- rest/test.py
+++
@@ -1,7 +0,0 @@
-def func(x, a, b, c):
-  return a*x*x + b*x + c
-
-X = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
-Y = [69, 48, 31, 18, 9, 4, 3, 6, 13, 24, 39]
-popt, pcov = scopt.curve_fit(func, X, Y)
-print(popt[0])
